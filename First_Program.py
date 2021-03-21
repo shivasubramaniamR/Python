a9c13cdca004a7c0efec4cd6f39d22632a8a39c5
shivasubramaniamR <shiva.subramaniam85@gmail.com>
2021-03-21 17:13:26 +0530

    Changes to First_program and new content for Variables

--- First_Program.py
+++ First_Program.py
@@ -1,1 +1,11 @@
+# First Print Statement
 print("My First Program")
+print("3")
+print(3)
+print(4+5)
+print(4+6.0)
+print("3"+"8")
+print("3.0"+"8")
+print("3"+3)
+
+
---
+++ Variables.py
@@ -0,0 +1,12 @@
+#Variables
+#    Rules for Python variables:
+#     1. A variable name must start with a letter or the underscore character
+#     2. A variable name cannot start with a number
+#     3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+#     4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+
+# My first Addtion using Variables
+first_number = 5
+_second_Number = 10
+print(first_number+_second_Number)
